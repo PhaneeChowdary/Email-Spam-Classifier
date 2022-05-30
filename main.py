5a5b326ab55714f1bc7ac3a4199acff09912e112
PhaneeChowdary <phaneendrababugunturu4393@gmail.com>
2022-05-30 18:52:25 +0530

    App

---
+++ main.py
@@ -0,0 +1,21 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open("spam.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+
+def main():
+    st.title("Email Spam Classification.", )
+    st.subheader("Build with Python and Streamlit.")
+    msg = st.text_input("Input", placeholder = "Enter your mail content here.")
+    st.write("Your mail : "+msg)
+    if(st.button("Predict")):
+        data = [msg]
+        vector = vectorizer.transform(data).toarray()
+        pred = model.predict(vector)[0]
+        if(pred == 0):
+            st.success("Your mail is not SPAM.")
+        else: st.error("Your mail is a SPAM.")
+
+main()
